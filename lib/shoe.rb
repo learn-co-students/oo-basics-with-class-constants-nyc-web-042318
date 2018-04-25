c05a1a8290d4ccbb3906044f6acf94f794bac795
srajeev91 <github email address>
2018-04-25 11:54:58 -0400

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,16 @@
+require 'pry'
+
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = []
+
   def initialize(brand)
     @brand = brand
+    if BRANDS.index(brand) == nil
+      BRANDS << brand
+    end
   end
 
   def cobble
